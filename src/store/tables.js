3a50f281310cfb3c5d5874554fdad858d2dbdec2
Kun Zhang <feel@shaw.ca>
2024-10-03 06:22:08 -0600

    more tables added

---
+++ src/store/tables.js
@@ -0,0 +1,325 @@
+table5a: [
+    {
+      "ambientTemperature": 35,
+      "correctionFactors": {
+        "60": 0.91,
+        "75": 0.94,
+        "90": 0.96,
+        "105": 0.97,
+        "110": 0.97,
+        "125": 0.97,
+        "150": 0.98,
+        "200": 0.99,
+        "250": 0.99
+      }
+    },
+    {
+      "ambientTemperature": 40,
+      "correctionFactors": {
+        "60": 0.82,
+        "75": 0.88,
+        "90": 0.91,
+        "105": 0.93,
+        "110": 0.94,
+        "125": 0.95,
+        "150": 0.96,
+        "200": 0.97,
+        "250": 0.98
+      }
+    },
+    {
+      "ambientTemperature": 45,
+      "correctionFactors": {
+        "60": 0.71,
+        "75": 0.82,
+        "90": 0.87,
+        "105": 0.89,
+        "110": 0.90,
+        "125": 0.92,
+        "150": 0.94,
+        "200": 0.95,
+        "250": 0.97
+      }
+    },
+    {
+      "ambientTemperature": 50,
+      "correctionFactors": {
+        "60": 0.58,
+        "75": 0.75,
+        "90": 0.82,
+        "105": 0.86,
+        "110": 0.87,
+        "125": 0.89,
+        "150": 0.91,
+        "200": 0.94,
+        "250": 0.95
+      }
+    },
+    {
+      "ambientTemperature": 55,
+      "correctionFactors": {
+        "60": 0.41,
+        "75": 0.67,
+        "90": 0.76,
+        "105": 0.82,
+        "110": 0.83,
+        "125": 0.86,
+        "150": 0.89,
+        "200": 0.92,
+        "250": 0.94
+      }
+    },
+    {
+      "ambientTemperature": 60,
+      "correctionFactors": {
+        "60": null,
+        "75": 0.58,
+        "90": 0.71,
+        "105": 0.77,
+        "110": 0.79,
+        "125": 0.83,
+        "150": 0.87,
+        "200": 0.91,
+        "250": 0.93
+      }
+    },
+    {
+      "ambientTemperature": 65,
+      "correctionFactors": {
+        "60": null,
+        "75": 0.47,
+        "90": 0.65,
+        "105": 0.73,
+        "110": 0.75,
+        "125": 0.79,
+        "150": 0.84,
+        "200": 0.89,
+        "250": 0.92
+      }
+    },
+    {
+      "ambientTemperature": 70,
+      "correctionFactors": {
+        "60": null,
+        "75": 0.33,
+        "90": 0.58,
+        "105": 0.68,
+        "110": 0.71,
+        "125": 0.76,
+        "150": 0.82,
+        "200": 0.87,
+        "250": 0.90
+      }
+    },
+    {
+      "ambientTemperature": 75,
+      "correctionFactors": {
+        "60": null,
+        "75": null,
+        "90": 0.50,
+        "105": 0.63,
+        "110": 0.66,
+        "125": 0.73,
+        "150": 0.79,
+        "200": 0.86,
+        "250": 0.89
+      }
+    },
+    {
+      "ambientTemperature": 80,
+      "correctionFactors": {
+        "60": null,
+        "75": null,
+        "90": 0.41,
+        "105": 0.58,
+        "110": 0.61,
+        "125": 0.69,
+        "150": 0.76,
+        "200": 0.84,
+        "250": 0.88
+      }
+    },
+    {
+      "ambientTemperature": 90,
+      "correctionFactors": {
+        "60": null,
+        "75": null,
+        "90": null,
+        "105": 0.45,
+        "110": 0.50,
+        "125": 0.61,
+        "150": 0.71,
+        "200": 0.80,
+        "250": 0.85
+      }
+    },
+    {
+      "ambientTemperature": 100,
+      "correctionFactors": {
+        "60": null,
+        "75": null,
+        "90": null,
+        "105": 0.26,
+        "110": 0.35,
+        "125": 0.51,
+        "150": 0.65,
+        "200": 0.77,
+        "250": 0.83
+      }
+    },
+    {
+      "ambientTemperature": 110,
+      "correctionFactors": {
+        "60": null,
+        "75": null,
+        "90": null,
+        "105": null,
+        "110": null,
+        "125": 0.40,
+        "150": 0.58,
+        "200": 0.73,
+        "250": 0.80
+      }
+    },
+    {
+      "ambientTemperature": 120,
+      "correctionFactors": {
+        "60": null,
+        "75": null,
+        "90": null,
+        "105": null,
+        "110": null,
+        "125": 0.23,
+        "150": 0.50,
+        "200": 0.69,
+        "250": 0.77
+      }
+    },
+    {
+      "ambientTemperature": 130,
+      "correctionFactors": {
+        "60": null,
+        "75": null,
+        "90": null,
+        "105": null,
+        "110": null,
+        "125": null,
+        "150": 0.41,
+        "200": 0.64,
+        "250": 0.74
+      }
+    },
+    {
+      "ambientTemperature": 140,
+      "correctionFactors": {
+        "60": null,
+        "75": null,
+        "90": null,
+        "105": null,
+        "110": null,
+        "125": null,
+        "150": 0.29,
+        "200": 0.59,
+        "250": 0.71
+      }
+    }
+  ]
+  table5b: [
+    {
+      "numberOfConductors": 2,
+      "correctionFactor": 0.90
+    },
+    {
+      "numberOfConductors": 3,
+      "correctionFactor": 0.85
+    },
+    {
+      "numberOfConductors": 4,
+      "correctionFactor": 0.80
+    }
+  ]
+  table5c: [
+    {
+      "numberOfConductors": "1-3",
+      "correctionFactor": 1.00
+    },
+    {
+      "numberOfConductors": "4-6",
+      "correctionFactor": 0.80
+    },
+    {
+      "numberOfConductors": "7-24",
+      "correctionFactor": 0.70
+    },
+    {
+      "numberOfConductors": "25-42",
+      "correctionFactor": 0.60
+    },
+    {
+      "numberOfConductors": "43 and up",
+      "correctionFactor": 0.50
+    }
+  ]
+  table5d: [
+    {
+      "numberOfConductorsHorizontally": 1,
+      "numberOfConductorsVertically": 1,
+      "correctionFactor": 1.00
+    },
+    {
+      "numberOfConductorsHorizontally": 2,
+      "numberOfConductorsVertically": 1,
+      "correctionFactor": 0.93
+    },
+    {
+      "numberOfConductorsHorizontally": 3,
+      "numberOfConductorsVertically": 1,
+      "correctionFactor": 0.87
+    },
+    {
+      "numberOfConductorsHorizontally": 4,
+      "numberOfConductorsVertically": 1,
+      "correctionFactor": 0.84
+    },
+    {
+      "numberOfConductorsHorizontally": 5,
+      "numberOfConductorsVertically": 1,
+      "correctionFactor": 0.83
+    },
+    {
+      "numberOfConductorsHorizontally": 6,
+      "numberOfConductorsVertically": 1,
+      "correctionFactor": 0.82
+    },
+    {
+      "numberOfConductorsHorizontally": 1,
+      "numberOfConductorsVertically": 2,
+      "correctionFactor": 0.89
+    },
+    {
+      "numberOfConductorsHorizontally": 2,
+      "numberOfConductorsVertically": 2,
+      "correctionFactor": 0.83
+    },
+    {
+      "numberOfConductorsHorizontally": 3,
+      "numberOfConductorsVertically": 2,
+      "correctionFactor": 0.79
+    },
+    {
+      "numberOfConductorsHorizontally": 4,
+      "numberOfConductorsVertically": 2,
+      "correctionFactor": 0.76
+    },
+    {
+      "numberOfConductorsHorizontally": 5,
+      "numberOfConductorsVertically": 2,
+      "correctionFactor": 0.75
+    },
+    {
+      "numberOfConductorsHorizontally": 6,
+      "numberOfConductorsVertically": 2,
+      "correctionFactor": 0.74
+    }
+  ]
+        
